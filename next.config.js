56f6f1f335405f6ee3f46c19ee626ed7b42cf951
Muhammed rasal vm <muhammedrasalvm999@gmail.com>
2024-01-12 16:56:43 +0530

    config added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+module.exports = {
+  basePath: "https://aesthetic-banoffee-e90a6f.netlify.app/",
+};
